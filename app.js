f2999e1c355e5210dbd6bacbec030a8f7e2c45de
Esteban TEMPLIER <esteban.templier82@gmail.com>
2023-09-18 00:46:39 +0200

    Base de donnée configuré, mise en place des routes post:, get:, put:, delete: pour le CRUD)

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require('mongoose');
+const Book = require('./models/Book');
+
+mongoose.connect('mongodb+srv://esteban:RPnznb81YjyDMKi2@cluster0.3m1xbab.mongodb.net/?retryWrites=true&w=majority',
+{ useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -9,40 +17,38 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/api/books', (req, res, next) => {
-    const BOOKS = [
-      {
-        userId: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        author: 'Anonimous',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        year: 2012,
-        genre: 'fiction',
-        ratings: [
-            {
-                userId:'oeihfzeoi',
-                grade:4,
-            }
-        ],
-        averageRating:4,
-      },
-      {
-        userId: 'oeihfzeoi',
-        title: 'Mon second objets',
-        author: 'Anonimous',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        year: 2015,
-        genre: 'aventure',
-        ratings: [
-            {
-                userId:'oeihfzeoi',
-                grade:5,
-            }
-        ],
-        averageRating:5,
-      },
-    ];
-    res.status(200).json(BOOKS);
+app.post('/api/books', (req, res, next) => {
+  delete req.body._id;
+  const book = new Book({
+    ...req.body
   });
+  book.save()
+  .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+  .catch(error => res.status(400).json({ error }));
+})
+
+app.put('/api/books/:id', (req, res, next) => {
+  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+  .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+  .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/books/:id', (req, res, next) => {
+  Book.deleteOne({ _id: req.params.id })
+  .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+  .catch(error => res.status(400).json({ error }));
+}); 
+
+app.get('/api/books/:id', (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+  .then(book => res.status(200).json(book))
+  .catch(error => res.status(404).json({ error }));
+}); 
+
+app.get('/api/books', (req, res, next) => {
+  Book.find()
+  .then(books => res.status(200).json(books))
+  .catch(error => res.status(400).json({ error }));
+}); 
 
 module.exports = app;
